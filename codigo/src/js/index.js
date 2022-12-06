78e4eb41dc2878c8c96004ff0d01482caba7f8eb
LuisMSouza <l.miguel-souza@hotmail.com>
2022-12-05 23:18:38 -0300

    commit updates

--- codigo/src/js/index.js
+++ codigo/src/js/index.js
@@ -9,6 +9,35 @@ window.onload = () => {
         } else instrucoes.innerHTML = '';
     };
 };
+
+function initTeams() {
+    const teams = [
+        { name: "Palmeiras", id: "56" },
+        { name: "Internacional", id: "44" },
+        { name: "Fluminense", id: "26" },
+        { name: "Corinthians", id: "65" },
+        { name: "Flamengo", id: "18" },
+        { name: "Athletico-PR", id: "185" },
+        { name: "Atlético-MG", id: "30" },
+        { name: "Fortaleza", id: "131" },
+        { name: "São Paulo", id: "57" },
+        { name: "América-MG", id: "33" },
+        { name: "Botafogo", id: "22" },
+        { name: "Santos", id: "63" },
+        { name: "Goiás", id: "115" },
+        { name: "Bragantino", id: "64" },
+        { name: "Coritiba", id: "84" },
+        { name: "Cuiabá", id: "204" },
+        { name: "Ceará", id: "105" },
+        { name: "Atlético-GO", id: "98" },
+        { name: "Avaí", id: "6" },
+        { name: "Juventude", id: "43" }
+    ]
+
+
+    localStorage.setItem('teams', JSON.stringify(teams));
+}
+
 function login() {
     let loggd;
     if (localStorage.getItem('token') == null) {
@@ -29,6 +58,7 @@ function login() {
 }
 
 login();
+initTeams();
 
 async function LoginUser() {
     window.location.href = "login.html"
@@ -38,4 +68,10 @@ async function sair() {
     localStorage.removeItem('token');
     localStorage.removeItem("userLogged");
     window.location.href = 'login.html';
+}
+
+function goToSearch() {
+    const query = document.getElementById("busca")
+
+    window.location.href = `busca.html?search=${query.value}`
 }
--- codigo/src/js/search.js
+++ codigo/src/js/search.js
@@ -1,5 +1,63 @@
+const apiKey = "live_b04e910d5a24e78400ca728b6ea174";
+Search()
+
 async function Search() {
-    
+    let time;
+    let found;
+    const urlParams = new URLSearchParams(location.search)
+    let Id = urlParams.get('search');
+
+    let ids = JSON.parse(localStorage.getItem('teams'));
+
+    ids.forEach(element => {
+        if (element.name.toLowerCase() == Id.toLowerCase()) {
+            time = element.id;
+        }
+    })
+
+    const myHeaders = new Headers({
+        'Authorization': `Bearer ${apiKey}`
+    });
+
+    const myInit = {
+        method: 'GET',
+        headers: myHeaders
+    };
+
+    const myRequest = new Request(`https://api.api-futebol.com.br/v1/times/${time}/partidas/anteriores`, myInit);
+
+    const results = await fetch(myRequest);
+    const data = await results.json();
+
+    let newArray = [];
+
+    Object.keys(data).forEach((item) => {
+        newArray.push(data[item])
+    });
+
+    let content = `
+    <div class="title-soccer">
+        <h1>Resultados para futebol</h1>
+    </div>`
+        ;
+
+    for (let i = 0; i < 10; i++) {
+        var escudoMandante = newArray[0][i].time_mandante.escudo;
+        var escudoVisitante = newArray[0][i].time_visitante.escudo;
+
+        content += `
+    <div class="result">
+            <h3><img src="${escudoMandante}"
+                    alt="My Happy SVG" width="20px" /> ${newArray[0][i].time_mandante.nome_popular} x
+                <img src="${escudoVisitante}"
+                    alt="My Happy SVG" width="20px" /> ${newArray[0][i].time_visitante.nome_popular}
+            </h3>
+        <p>${newArray[0][i].data_realizacao} - ${newArray[0][i].hora_realizacao} - ${newArray[0][i].placar} - ${newArray[0][i].status}</p>
+    </div>
+        `
+
+        document.querySelector(".results-soccer").innerHTML = content;
+    }
 }
 
 async function Back() {
